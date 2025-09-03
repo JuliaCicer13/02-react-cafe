629bdf45d4e3aab039f6c497fc1596312a258cf1
Yuliia <ciceron131313@gmail.com>
2025-09-04 02:09:27 +0300

    k

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import css from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo";
+
+export default function App() {
+     <div className={css.app}>
+          return (
+            <>
+              <CafeInfo />
+            </>
+          )
+       </div>
+}
+
+
+
+
+
